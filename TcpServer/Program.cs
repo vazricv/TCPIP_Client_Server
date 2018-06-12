27a16130706c08cdc6d19dbbb3bd794aa1a18b9c
DESKTOP-9H6848J\vkara <vahe.karamian@noorcon.com>
2018-06-11 20:15:20 -0700

    TCP Client/Server Code.

---
+++ TcpServer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace NoorconTcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+                // use local m/c IP address, and 
+                // use the same in the client
+
+                /* Initializes the Listener */
+                TcpListener myList = new TcpListener(ipAd, 8001);
+
+                /* Start Listeneting at the specified port */
+                myList.Start();
+
+                Console.WriteLine("The server is running at port 8001...");
+                Console.WriteLine("The local End point is  :" +
+                                  myList.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection.....");
+
+                Socket s = myList.AcceptSocket();
+                while(true)
+                {
+                    Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+                    byte[] b = new byte[100];
+                    int k = s.Receive(b);
+                    Console.WriteLine("Recieved...");
+                    string msg = "";
+                    for (int i = 0; i < k; i++)
+                    {
+                        msg += Convert.ToChar(b[i]);
+                        Console.Write(Convert.ToChar(b[i]));
+                    }
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    s.Send(asen.GetBytes("The string was recieved by the server."));
+                    Console.WriteLine("\nSent Acknowledgement");
+                    /* clean up */
+                    if(msg.Contains("END"))
+                    {
+                        s.Close();
+                        myList.Stop();
+                        return;
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+        }
+    }
+}
